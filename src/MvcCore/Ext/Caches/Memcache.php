eca62bee18fa48c53634e9f284fb1e558678c59a
Tom Flídr <tomflidr@gmail.com>
2024-04-02 14:55:50 +0200

    release 5.2.1

--- src/MvcCore/Ext/Caches/Memcache.php
+++ src/MvcCore/Ext/Caches/Memcache.php
@@ -43,7 +43,7 @@ implements	\MvcCore\Ext\ICache {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.0';
+	const VERSION = '5.2.1';
 
 	/**
 	 * Provider configuration keys.
